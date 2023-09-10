ddb6f8cfa1577163639e949873c1f76a8e9b459f
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-10 01:04:55 +0100

    Initiasl commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program {
+    public static void Main(string[] args) {
+
+        string[] ReadInData = File.ReadAllLines("students.csv");
+
+        int rows = ReadInData.Length;
+        int columns = 2;
+
+        string[,] Data = new string[rows, columns];
+        
+        string[] temp = new string[columns];
+
+        for(int i = 0; i < rows; i++) {
+
+            for(int j = 0; j < columns; j++) {
+                Data[i,j] = ReadInData[i].Split(',')[j];
+            }
+        }
+
+    }
+}
